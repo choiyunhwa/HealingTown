6ba84d7ccba5c5ca23b0adc64a54fd264589a686
choiyunhwa <sjo4208@naver.com>
2024-05-12 03:57:26 +0900

    Fix > ChoicePlayerVIew

---
+++ Assets/02_Scripts/UI/ChoicePlayerView.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChoicePlayerView : UIView
+{
+    public static Action PlayerUpdateEvent;
+
+    public VisualElement playerList;
+
+    private VisualTreeAsset playerFrame;
+
+    public ChoicePlayerView(VisualElement _root) : base(_root)
+    {
+        playerFrame = Resources.Load<VisualTreeAsset>("CharacterChoiceTemp");
+        PlayerUpdateEvent += UpdateCharacterData;
+    }
+
+    protected override void SetVisualElement()
+    {
+        base.SetVisualElement();
+        playerList = root.Q<VisualElement>("CharcterListView");
+    }
+
+    protected override void RegisterButtonCallback()
+    {
+        base.RegisterButtonCallback();
+    }
+
+    private void UpdateCharacterData()
+    {
+        CreatePlayerImg(playerList);
+    }
+
+    private void CreatePlayerImg(VisualElement parentVisual)
+    {
+        if (playerFrame == null)
+            return;
+
+        //https://docs.unity3d.com/ScriptReference/UIElements.TemplateContainer.html
+        TemplateContainer playerElem = playerFrame.Instantiate();
+
+        VisualElement img = playerElem.Q<VisualElement>("Character_Img");
+
+        img.RegisterCallback<ClickEvent>((evt) =>
+        {
+            //캐릭터 정보 보내기
+        });
+
+        parentVisual.Add(playerElem);
+    }
+}
---
+++ Assets/02_Scripts/UI/PlayerInforSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Assets/Resources/Player/PlayerData", menuName = "PlayerData", order = 0)]
+public class PlayerInforSO : ScriptableObject
+{
+    public AnimationController controller;
+
+}
